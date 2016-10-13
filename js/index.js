54dcd4d6e628d797a637567db7c20e418be7344b
“503305196@qq.com <503305196@qq.com>
2016-10-13 13:54:57 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,82 @@
+var V = new Vue({
+	el: "#app",
+	data: {
+		todeMess: "",
+		list: [],
+		search: "all"
+	},
+	methods: {
+
+		/*添加*/
+		addTodo: function() {
+			if(this.todeMess != '') {
+				this.list.push({
+					mess: this.todeMess,
+					status: 0
+				});
+				this.todeMess = ""
+			}
+
+		},
+
+		/*完成任务*/
+		check: function(item) {
+			if(item.status) {
+				item.status = 0;
+			} else {
+				item.status = 1;
+			}
+			this.setLocal();
+		},
+
+		/*删除任务*/
+		remove: function(index) {
+			this.list.splice(index, 1);
+		},
+		
+		clear:function(){
+			this.list = [];
+			this.search = "all"
+		},
+
+		/*获取存储本地的数据*/
+		getLocal: function() {
+			this.list = JSON.parse(localStorage.getItem("list")) || [];
+		},
+
+		/*存储数据到本地*/
+		setLocal: function() {
+			localStorage.setItem("list", JSON.stringify(this.list));
+		}
+	},
+	watch: {
+		'list': function() {
+			this.setLocal();
+		}
+	},
+	ready: function() {
+		this.getLocal();
+	},
+	filters: {
+
+		query: function(list) {
+			var items = [];
+			if(this.search == "yes") {
+				for(item in list) {
+					if(list[item].status == 1) {
+						items.push(list[item]);
+					}
+				}
+			} else if(this.search == "no") {
+				for(item in list) {
+					if(list[item].status == 0) {
+						items.push(list[item]);
+					}
+				}
+			} else {
+				items = list;
+			}
+			return items;
+		}
+	}
+})
